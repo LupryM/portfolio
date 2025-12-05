21b89896f9a9e2ea528f20f8f803ec7a634634a0
Lupry Munyai <luprymunyai@gmail.com>
2025-12-05 18:15:07 +0200

    Fixed Float Hero

--- lib/projects.ts
+++ lib/projects.ts
@@ -1,52 +1,56 @@
 export interface Project {
-    title: string;
-    description: string;
-    img: string;
-    url: string;
-    tags?: string[];
+  title: string;
+  description: string;
+  img: string;
+  url: string;
+  tags?: string[];
 }
 
 export const projects: Project[] = [
-    {
-        title: "MR FLOAT - Transport Service",
-        description: "A transport company in Zimbabwe that offers a wide array of services, including taxi rides, long-distance travel, car hire, airport shuttles, and package deliveries.",
-        img: "/preview/mr-float.png",
-        url: "https://private-car-hire.vercel.app",
-        tags: ["Transportation", "Booking"]
-    },
-    {
-        title: "Vuza Trading",
-        description: "A professional trading platform with real-time analytics and user-friendly interface.",
-        img: "/preview/vuza-trading.png",
-        url: "https://vuzatrading.com",
-        tags: ["Automated Security", "Aluminum Solutions", "CCTV", "Alarm Systems"]
-    },
-    {
-        title: "Mothupi & Lesego - Wedding",
-        description: "A wedding invitation with an interactive map and a gift registry.",
-        img: "/preview/mothupi-and-lesego.png",
-        url: "https://mothupi-and-lesego.co.za",
-        tags: ["Wedding", "Event"]
-    },
-    {
-        title: "Monare Selahle Portfolio",
-        description: "Modern portfolio for a Computer Science student.",
-        img: "/preview/monare-selahle.png",
-        url: "https://monareselahle.co.za",
-        tags: ["Software Engineer", "Portfolio"]
-    },
-    {
-        title: "Shots By Ozee",
-        description: "A photographer and visionary creative currently studying Chemical Engineering.",
-        img: "/preview/shots-by-ozee.png",
-        url: "https://shotsbyozee.co.za",
-        tags: ["Photography", "Portfolio"]
-    },
-    {
-        title: "Gordian Malaka",
-        description: "A film student who has a passion for photography.",
-        img: "/preview/gordian-malaka.png",
-        url: "https://uvhu-portfolio-9rw1.vercel.app/",
-        tags: ["Photography", "Videography","Portfolio"]
-    }
+  {
+    title: "MR FLOAT - Transport Service",
+    description:
+      "A transport company in Zimbabwe that offers a wide array of services, including taxi rides, long-distance travel, car hire, airport shuttles, and package deliveries.",
+    img: "/preview/Float.webp",
+    url: "https://private-car-hire.vercel.app",
+    tags: ["Transportation", "Booking"],
+  },
+  {
+    title: "Vuza Trading",
+    description:
+      "A professional trading platform with real-time analytics and user-friendly interface.",
+    img: "/preview/vuza-trading.png",
+    url: "https://vuzatrading.com",
+    tags: ["Automated Security", "Aluminum Solutions", "CCTV", "Alarm Systems"],
+  },
+  {
+    title: "Mothupi & Lesego - Wedding",
+    description:
+      "A wedding invitation with an interactive map and a gift registry.",
+    img: "/preview/mothupi-and-lesego.png",
+    url: "https://mothupi-and-lesego.co.za",
+    tags: ["Wedding", "Event"],
+  },
+  {
+    title: "Monare Selahle Portfolio",
+    description: "Modern portfolio for a Computer Science student.",
+    img: "/preview/monare-selahle.png",
+    url: "https://monareselahle.co.za",
+    tags: ["Software Engineer", "Portfolio"],
+  },
+  {
+    title: "Shots By Ozee",
+    description:
+      "A photographer and visionary creative currently studying Chemical Engineering.",
+    img: "/preview/shots-by-ozee.png",
+    url: "https://shotsbyozee.co.za",
+    tags: ["Photography", "Portfolio"],
+  },
+  {
+    title: "Gordian Malaka",
+    description: "A film student who has a passion for photography.",
+    img: "/preview/gordian-malaka.png",
+    url: "https://uvhu-portfolio-9rw1.vercel.app/",
+    tags: ["Photography", "Videography", "Portfolio"],
+  },
 ];
